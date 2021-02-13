05394e21fb2d0b79a7b3bfd6975853bde49fd2f9
Jagadish <jagadish88@gmail.com>
2021-02-14 00:33:52 +0530

    JS for mark palette

--- _includes/js/custom.js
+++ _includes/js/custom.js
@@ -1,6 +1,7 @@
 
 // Display the right image in the palette
 
+
 function display (selected) {
   if(selected!='none'){
       var path = "<img src=\"/assets/images/pics/";
@@ -9,5 +10,20 @@ function display (selected) {
       document.getElementById("thetext").innerHTML = texttoshow;
   }
 }
+
+
+
+function markdisplay (selected) {
+  if(selected!='none'){
+      var path = "<img src=\"/assets/images/mock_test/";
+      var title = "<h2>Submission spotlight</h2><h4><large>";
+      title = title.concat ( selected.replace("/",": ").replaceAll("_"," ").replace("/", "").replace(".jpg","").concat("</large></h4>") )
+      title = title.replace("PartA","Part A")
+      var texttoshow = " ";
+      texttoshow = path.concat(selected).concat(".jpg\" />");
+      document.getElementById("themarktext").innerHTML = title.concat(texttoshow);
+  }
+}
+
 
 
